5ad128c27ac807de5e62fc238ad6981d9ef81170
Xavier Sellier <contact@binogure-studio.com>
2024-02-26 10:24:23 +0100

    Clean up JS

--- js/interface.js
+++ js/interface.js
@@ -15,13 +15,6 @@ for (var index = 0; index < galleryNodes.length; ++index) {
   }).bind(document, index)
 }
 
-var newsletter_email = document.getElementById('newsletter-email')
-var newsletter = document.getElementById('newsletter-submit')
-var newsletter_form = document.getElementById('newsletter-form')
-
-newsletter_email.oninput = emailAddressHandler
-newsletter.onclick = subscrireToNewsletter
-
 modal.onclick = function() {
   if (!skipEvent) {
     modal.style.display = "none";
@@ -94,41 +87,5 @@ function update_scroll (event) {
       layer.style['-o-transform'] = translate3d
       layer.style.transform = translate3d
     }
-  }
-}
-
-function validateEmail(email) {
-  return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(email)
-}
-
-function emailAddressHandler() {
-  var emailInput = document.getElementById('newsletter-email')
-  var sendButton = document.getElementById('newsletter-submit')
-
-  sendButton.disabled = !validateEmail(emailInput.value)
-}
-
-function ajaxHandler() {
-  console.log(this.responseText);
-  // There is no need to check the status code since it _should_ always work
-  if (this.readyState == 4) {
-    document.getElementById('subscribed').style.display = 'block'
-  }
-}
-
-function subscrireToNewsletter() {
-  emailAddressHandler()
-
-  var emailInput = document.getElementById('newsletter-email')
-
-  // If the email address is valid
-  if (validateEmail(emailInput.value)) {
-    var xhttp = new XMLHttpRequest()
-
-    xhttp.onreadystatechange = ajaxHandler
-    xhttp.open('POST', 'https://www.mailinglist.fr/mlbox/form', true)
-    xhttp.setRequestHeader('Access-Control-Request-Headers', 'X-PINGOTHER, Content-Type')
-    xhttp.setRequestHeader('Access-Control-Request-Method', 'POST')
-    xhttp.send(new FormData(newsletter_form))
   }
 }
